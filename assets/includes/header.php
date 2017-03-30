8a17215be2d31cd89418cc6b7e2f9e9b637645af
John Nowinsky <armthepit@armthepit.com>
2017-03-30 12:41:36 -0500

    add style, header, navigation, and footer includes to use on the index.php. add logo to the images folder.

---
+++ assets/includes/header.php
@@ -0,0 +1,3 @@
+<header>
+	<img src="assets/images/header.png" alt="Rockin To America Logo" class="img-responsive center-block">
+</header>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Rockin To America</title>
+    <?php include("assets/includes/style.php");?>
+</head>
+
+<body>
+    <?php include_once("assets/includes/header.php");?>
+    <?php include_once("assets/includes/navigation.php");?>
+    <div class="content">
+        test
+    </div>
+    <?php include_once("assets/includes/footer.php");?>
+    <?php include("assets/includes/js.php");?>
+</body>
+
+</html>
